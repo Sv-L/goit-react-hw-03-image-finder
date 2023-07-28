30f85044f45255169daa6f9a31129189c450831a
liulka_s <asl.warmhaus@gmail.com>
2023-07-28 07:07:45 +0200

    image-finder

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,63 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Component } from 'react';
+import { AppWrap } from './App.styled';
+import { ToastContainer } from 'react-toastify';
+import Searchbar from './Searchbar';
+import Modal from './Modal';
+import Button from './Button';
+import ImageGallery from './ImageGallery';
+import Loader from './Loader';
+import Error from './Error';
+
+export class App extends Component {
+  state = {
+    search: null,
+    loading: false,
+    currentPage: 1,
+    totalPage: null,
+    error: null,
+    images: [],
+    selectedImageIndex: null,
+  };
+
+  changeStateCallback = stateCallback => {
+    this.setState(prevState => stateCallback(prevState));
+  };
+
+  render() {
+    const {
+      images,
+      loading,
+      selectedImageIndex,
+      search,
+      currentPage,
+      totalPage,
+      error,
+    } = this.state;
+    const { changeStateCallback } = this;
+    return (
+      <AppWrap>
+        <Searchbar onSubmit={changeStateCallback} />
+        {error && <Error message={error.message} />}
+        <ImageGallery
+          changeStateCallback={changeStateCallback}
+          search={search}
+          page={currentPage}
+          images={images}
+          loading={loading}
+        />
+        {loading && <Loader />}
+        {totalPage > currentPage && (
+          <Button onClick={changeStateCallback}>Load More</Button>
+        )}
+        {(selectedImageIndex || selectedImageIndex === 0) && (
+          <Modal
+            onClose={changeStateCallback}
+            imgInd={selectedImageIndex}
+            images={images}
+          />
+        )}
+        <ToastContainer autoClose={2000} />
+      </AppWrap>
+    );
+  }
+}
